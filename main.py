28f2f8f85600423bc2db979bfd5809ee0bfe4454
juliaedwiges <74474146+juliaedwiges@users.noreply.github.com>
2025-05-31 17:55:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ def preprocess_image(image_bytes):
     image = image.resize((224, 224))
     img_data = np.array(image).astype(np.float32) / 255.0
     img_data = np.transpose(img_data, (2, 0, 1))
+
+    # Normalização igual ao treino PyTorch
+    mean = np.array([0.485, 0.456, 0.406]).reshape(3,1,1)
+    std = np.array([0.229, 0.224, 0.225]).reshape(3,1,1)
+    img_data = (img_data - mean) / std
+
     img_data = np.expand_dims(img_data, axis=0)
     return img_data
 
@@ -26,9 +32,13 @@ async def predict(file: UploadFile = File(...)):
     input_tensor = preprocess_image(image_bytes)
     input_name = session.get_inputs()[0].name
     outputs = session.run(None, {input_name: input_tensor})
+
     probs = outputs[0][0]
+    print("Probabilidades:", probs)  # Para debug no console do Render
+
     predicted_class = int(np.argmax(probs))
     confidence = float(np.max(probs))
+
     return JSONResponse(content={
         "predicted_class": predicted_class,
         "confidence": confidence
